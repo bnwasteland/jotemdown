84bb593fc8ffea7d05c0cdf8986395ed3864edcd
Jace Bennett <jace.bennett@gmail.com>
2014-08-04 00:57:40 -0400

    remove foreign code that was mainly for convenient cross-reference

--- src/jotemdown/core.clj
+++ src/jotemdown/core.clj
@@ -1,36 +1,6 @@
 (ns jotemdown.core
   (:require [instaparse.core :as insta]))
 
-;; This method is from [Hitman][https://github.com/chameco/Hitman], but it
-;; doesn't cover spans (those are done with post-processing), and it's very line oriented.
-(def parse-md
-  (insta/parser
-    "<Blocks> = (Paragraph | Header | List | Ordered | Code | Rule)+
-    Header = Line Headerline Blankline+
-    <Headerline> = h1 | h2
-    h1 = '='+
-    h2 = '-'+
-    List = Listline+ Blankline+
-    Listline = Listmarker Whitespace+ Word (Whitespace Word)* EOL
-    <Listmarker> = <'+' | '*' | '-'>
-    Ordered = Orderedline+ Blankline+
-    Orderedline = Orderedmarker Whitespace* Word (Whitespace Word)* EOL
-    <Orderedmarker> = <#'[0-9]+\\.'>
-    Code = Codeline+ Blankline+
-    Codeline = <Space Space Space Space> (Whitespace | Word)* EOL
-    Rule = Ruleline Blankline+
-    <Ruleline> = <'+'+ | '*'+ | '-'+>
-    Paragraph = Line+ Blankline+
-    <Blankline> = Whitespace* EOL
-    <Line> = Linepre Word (Whitespace Word)* Linepost EOL
-    <Linepre> = (Space (Space (Space)? )? )?
-    <Linepost> = Space?
-    <Whitespace> = #'(\\ | \\t)+'
-    <Space> = ' '
-    <Word> = #'\\S+'
-    <EOL> = <'\\n'>"))
-
-
 (def parse-jotdown
   (insta/parser
     "<Blocks> = (Paragraph)+
